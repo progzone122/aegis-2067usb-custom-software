4bd23d94b2c1e7d437ef3127bb76b5b762e5fa79
diablo <progzone122@gmail.com>
2024-10-31 13:19:07 +0200

    ..

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,21 @@
-use clap::ArgMatches;
-use api::Api;
-use api::led::LED;
 use anyhow::{Context, Result};
+use api::led::LED;
+use api::Api;
+use clap::ArgMatches;
 
 macro_rules! define_animation_subcommand {
     () => {
         clap::Command::new("animation")
-            // animation name / animation id
+            .arg(
+                clap::Arg::new("value")
+                    .required(true)
+                    .index(1),
+            )
+    };
+}
+macro_rules! define_brightness_subcommand {
+    () => {
+        clap::Command::new("brightness")
             .arg(
                 clap::Arg::new("value")
                     .required(true)
@@ -21,6 +30,7 @@ fn main() -> Result<()> {
         .author(env!("CARGO_PKG_AUTHORS"))
         .about(env!("CARGO_PKG_DESCRIPTION"))
         .subcommand(define_animation_subcommand!())
+        .subcommand(define_brightness_subcommand!())
         .get_matches();
 
     let api: Api = Api::default();
@@ -30,25 +40,50 @@ fn main() -> Result<()> {
 
     match device {
         Ok(interface) => {
-            println!("[INFO] Successfully connected to device with VID {:04X} and PID {:04X}", api.vid, api.pid);
+            println!(
+                "[INFO] Successfully connected to device with VID {:04X} and PID {:04X}",
+                api.vid, api.pid
+            );
             match matches.subcommand() {
                 Some(("animation", sub_m)) => {
-                    let value: &str = sub_m.get_one::<String>("value")
+                    let value: &str = sub_m
+                        .get_one::<String>("value")
                         .expect("value argument is required")
                         .as_str();
 
                     if let Ok(id) = value.parse::<u8>() {
-                        if let Some(animation) = api::values::AnimationEffect::find_id(id as usize) {
-                            led_settings.change_animation_effect(interface, animation.hex)
-                                .with_context(|| format!("Failed to change animation effect to ID {}", id))?;
+                        if let Some(animation) = api::values::AnimationEffect::find_id(id as usize)
+                        {
+                            led_settings
+                                .change_animation_effect(interface, animation.hex)
+                                .with_context(|| {
+                                    format!("Failed to change animation effect to ID {}", id)
+                                })?;
                         } else {
                             eprintln!("[ERROR] Animation effect not found for ID: {}", id);
                         }
                     } else if let Some(animation) = api::values::AnimationEffect::find_name(value) {
-                        led_settings.change_animation_effect(interface, animation.hex)
-                            .with_context(|| format!("Failed to change animation effect to name {}", value))?;
+                        led_settings
+                            .change_animation_effect(interface, animation.hex)
+                            .with_context(|| {
+                                format!("Failed to change animation effect to name {}", value)
+                            })?;
                     } else {
                         eprintln!("[ERROR] Could not find animation effect: {}", value);
+                    }
+                }
+                Some(("brightness", sub_m)) => {
+                    let value: &str = sub_m
+                        .get_one::<String>("value")
+                        .expect("value argument is required")
+                        .as_str();
+
+                    if let Ok(brightness) = value.parse::<u8>() {
+                        led_settings
+                            .change_brightness(interface, brightness)
+                            .with_context(|| {
+                                format!("Failed to change brightness {}", brightness)
+                            })?;
                     }
                 }
                 None => {
